daaf0688a2948cfc4d73c0a9e6de2e6bc7e5f67a
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-21 18:11:00 +0100

    add array di oggetti e v-for

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,62 @@
 <script>
 export default {
     name: "AppHeader",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -13,14 +69,9 @@ export default {
         <!-- navigazione menù principale -->
         <nav>
             <ul>
-                <li>
-                    <a href="" class="active">
-                        testo
-                    </a>
-                </li>
-                <li>
-                    <a href="" class="">
-                        testo
+                <li v-for="link in links">
+                    <a :href="link.url" :class="link.current ? 'active' : ''">
+                        {{ link.text }}
                     </a>
                 </li>
             </ul>
