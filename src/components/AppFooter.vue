47760c1c5fc2b21e4f04c3da2fea8b4f4f040c35
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-21 18:41:38 +0100

    add html e css

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,49 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="top-footer">
+        <!-- questa è la parte con le 5 icone -->
+    </div>
+    <div class="link-section">
+        <!-- parte del footer coi vari link -->
+        <div class="link-container">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <div class="bottom-footer">
+        <!-- parte più in fondo del footer -->
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.top-footer {
+    background-color: #0882F9;
+    // debug
+    min-height: 100px;
+}
+
+.link-section {
+    background-image: url(../assets/img/footer-bg.jpg);
+    // debug
+    min-height: 350px;
+    padding-top: 40px;
+}
+
+.link-container {
+    display: flex;
+    // debug
+    min-height: 250px;
+    background-color: orange;
+    width: 30%;
+    margin-left: 150px;
+
+    .col {
+        border: 1px solid black;
+        // debug
+        width: calc(100% / 3);
+    }
+}
+</style>
