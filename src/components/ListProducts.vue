c1ab5a6984b3c6bf293dd261d1ee5f17f652d2d0
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-21 17:31:47 +0100

    creazione

---
+++ src/components/ListProducts.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "ListProducts",
+}
+</script>
+
+<template>
+    <h2>Ciao sono il Main</h2>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+</style>
