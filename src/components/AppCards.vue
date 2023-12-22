60e05bf1eade3bd1fd10d43d549a56ccb53c69e2
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-22 15:57:03 +0100

    add props per cards + html + css

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,19 +1,39 @@
 <script>
 export default {
+    props: {
+        cards: Array
+    },
     name: "AppCards",
 }
 </script>
 
 <template>
-    <div class="card-container">
-
+    <div class="prod-card">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="img">
+        <h3>Titolo</h3>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.card-container {
-    background-color: orange;
-    min-height: 200px;
-    width: 200px;
+@use '../styles/partials/variables' as *;
+
+
+
+.prod-card {
+    background-color: white;
+    border: 2px solid black;
+    display: inline-block;
+    height: auto;
+    width: calc((100% / 5) - 10px);
+    margin: 5px;
+
+    h3 {
+        color: $primary;
+    }
+
+    img {
+        width: 100%;
+        height: auto;
+    }
 }
 </style>
